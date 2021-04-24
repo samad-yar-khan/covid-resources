3235dd66925cef414218fc0336042cdc94a1f06d
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-04-24 21:31:29 +0530

    Project Setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '83692c4367c39479b517f175bddf3fe9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fce1f33d-ed6f-4746-a141-993476a50193'
+};
